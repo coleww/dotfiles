79c229fcf481d2cc76a378cde2340734c2b0e7a6
Cole Willsea <coleww@gmail.com>
2015-07-23 11:26:50 -0700

    fix JS and tabs => spaces

--- .live-packs/colewillsea-pack/config/bindings.el
+++ .live-packs/colewillsea-pack/config/bindings.el
@@ -4,3 +4,6 @@
 ;;(define-key global-map (kbd "C-+") 'text-scale-increase)
 ;;(define-key global-map (kbd "C--") 'text-scale-decrease)
 (global-set-key [f8] 'neotree-toggle)
+
+(setq js-indent-level 2)
+(setq-default indent-tabs-mode nil)
